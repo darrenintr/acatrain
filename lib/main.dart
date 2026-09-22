2ac506510a8ba3139aa5d7f26f4c37183103b5c8
Darren <168272439+darrenintr@users.noreply.github.com>
2026-09-22 21:46:34 +0800

    fix: import Cupertino page transition builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_displaymode/flutter_displaymode.dart';
